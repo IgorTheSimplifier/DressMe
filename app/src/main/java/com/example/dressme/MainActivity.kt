570c1084ca402b51d47c494a2adc3cdf58e14ccb
bboyifeel <bboyifeel@gmail.com>
2019-11-07 13:22:40 +0100

    login or Register and Redirection

--- app/src/main/java/com/example/dressme/MainActivity.kt
+++ app/src/main/java/com/example/dressme/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.dressme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn_click_me = findViewById(R.id.buttonToLogin) as Button
+        login_button.setOnClickListener{
+            // Handler code here.
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent);
+        }
 
-        btn_click_me.setOnClickListener {
-            // your code to perform when the user clicks on the button
-            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        signup_button.setOnClickListener{
+            // Handler code here.
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent);
         }
 
     }
