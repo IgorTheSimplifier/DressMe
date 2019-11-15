2a2e0a599cacdd42352949f8968411d81c52f00a
Igor <bboyifeel@gmail.com>
2019-11-15 08:26:40 +0100

    + firestore user name upload

--- app/src/main/java/com/example/dressme/SignupActivity.kt
+++ app/src/main/java/com/example/dressme/SignupActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
@@ -38,6 +40,7 @@ class SignupActivity : AppCompatActivity() {
                 if (!it.isSuccessful) return@addOnCompleteListener
 
                 Log.d(TAG, "Signup has succeeded ${it.result?.user?.uid}")
+                saveUserToFirebaseDatabase()
 
                 val intent = Intent(this, ProfileMainActivity::class.java)
                 startActivity(intent);
@@ -48,19 +51,44 @@ class SignupActivity : AppCompatActivity() {
     }
 
 
+    private fun saveUserToFirebaseDatabase() {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseFirestore.getInstance().collection("users")
+
+        val name            = name_edittext_signup.text.toString()
+        val email           = email_edittext_signup.text.toString()
+
+        val user = User(name, email)
+
+        ref.document(uid).set(user)
+            .addOnSuccessListener {
+                Log.d(TAG, "Our user has been saved to Firebase Database")
+            }
+            .addOnFailureListener {
+                Log.w(TAG, "Error adding document ", it)
+            }
+
+    }
+
     // $todo: signup validation update
     private fun valid(): Boolean {
+        val name            = name_edittext_signup.text.toString()
         val email           = email_edittext_signup.text.toString()
         val password        = password_edittext_signup.text.toString()
         val passwordReenter = password_reenter_edittext_signup.text.toString()
 
+        Log.d(TAG, "Name is  $name")
         Log.d(TAG, "Email is  $email")
         Log.d(TAG, "Password is  $password")
         Log.d(TAG, "Password Reenter is  $passwordReenter")
 
-        if (email.isEmpty() || password.isEmpty() || password != passwordReenter)
-            return false
+        if (name.isEmpty() ||
+            email.isEmpty() ||
+            password.isEmpty() ||
+            password != passwordReenter) return false
 
         return true
     }
 }
+
+class User(val name: String, val email: String)
