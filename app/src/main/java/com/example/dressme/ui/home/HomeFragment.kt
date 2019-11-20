7b6f320f3c4e6dc790d9c8717e2715e9fd93fb55
Igor <bboyifeel@gmail.com>
2019-11-20 17:56:36 +0100

    open item tmp solution

--- app/src/main/java/com/example/dressme/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/dressme/ui/home/HomeFragment.kt
@@ -1,14 +1,20 @@
 package com.example.dressme.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
+import androidx.constraintlayout.widget.Group
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.dressme.ItemViewActivity
+import com.example.dressme.ProfileMainActivity
 import com.example.dressme.R
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +32,12 @@ class HomeFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         homeViewModel.text.observe(this, Observer {
         })
+
+        root.imageView21.setOnClickListener{
+            val intent = Intent(context, ItemViewActivity::class.java)
+            startActivity(intent);
+        }
+
         return root
     }
 }
