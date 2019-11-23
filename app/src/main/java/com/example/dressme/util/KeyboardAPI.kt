18274cbfeb23dc3b921a04662ea05e35547a3218
Igor <bboyifeel@gmail.com>
2019-11-23 15:42:09 +0100

    fixing warnings

--- app/src/main/java/com/example/dressme/util/KeyboardAPI.kt
+++ app/src/main/java/com/example/dressme/util/KeyboardAPI.kt
@@ -1,17 +1,12 @@
 package com.example.dressme.util
 
 import android.app.Activity
-import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
-
-
-
 
 class KeyboardAPI {
     companion object {
-        public fun hideKeyboard(activity: Activity) {
+        fun hideKeyboard(activity: Activity) {
             val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
             //Find the currently focused view, so we can grab the correct window token from it.
             var view = activity.currentFocus
@@ -19,11 +14,6 @@ class KeyboardAPI {
             if (view == null) {
                 view = View(activity)
             }
-            imm!!.hideSoftInputFromWindow(view!!.windowToken, 0)
-        }
-
-        fun hideKeyboardFrom(context: Context, view: View) {
-            val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
     }
