858045f5b81e7f4e40f02e5358551827e1da8908
Igor <bboyifeel@gmail.com>
2019-12-02 00:28:22 +0100

    item selected back + fast forward launch

--- app/src/main/java/com/example/dressme/InspectItemActivity.kt
+++ app/src/main/java/com/example/dressme/InspectItemActivity.kt
@@ -2,11 +2,21 @@ package com.example.dressme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class InspectItemActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inspect_item)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+    
 }
